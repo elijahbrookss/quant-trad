663c1ab65eb5244dfeddf5885511b1d92568bdb3
Elijah Brooks <elijahcbrooks@gmail.com>
2025-05-23 08:52:46 -0500

    updated ohlc schema and added auditing columns

--- data_providers/base.py
+++ data_providers/base.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from enum import Enum
 import datetime as dt
 import pandas as pd
 
@@ -15,6 +16,18 @@ class BaseDataProvider(ABC):
         Retrieve OHLCV data for the given symbol between start and end datetimes.
 
         Returns a DataFrame with the following required columns:
-        ['ts', 'open', 'high', 'low', 'close', 'volume', 'symbol']
+        ['ts', 'open', 'high', 'low', 'close', 'volume', 'symbol', 'datasource']
         """
         pass
+
+    @abstractmethod
+    def get_datasource(self) -> str:
+        """
+        Return the name of the data source as a string.
+        """
+        pass
+
+class DataSource(str, Enum):
+    YFINANCE = "YFINANCE"
+    ALPACA = "ALPACA"
+    UNKNOWN = "UNKNOWN"
