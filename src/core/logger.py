680e7df2800530d4da740a206519e166bc43b7fd
Elijah Brooks <elijahcbrooks@gmail.com>
2025-07-16 19:34:00 -0500

    setup environmnet variables and configurations for tests

--- src/core/logger.py
+++ src/core/logger.py
@@ -1,5 +1,12 @@
 import logging
 from src.utils.logging_utils import LokiHandler, ExcludeLoggerFilter
+from dotenv import load_dotenv
+
+load_dotenv("secrets.env")
+load_dotenv(".env")
+
+debug_mode = True if os.getenv("DEBUG", "false").lower() == "true" else False
+env = os.getenv("ENV", "dev").lower()
 
 LOG_FMT = "%(asctime)s %(levelname)-5s %(filename)s:%(lineno)d | %(message)s"
 logging.basicConfig(level=logging.DEBUG, format=LOG_FMT)
@@ -8,7 +15,7 @@ LOKI_URL = "http://localhost:3100"
 LOKI_LABELS = {"app": "quant_trad", "env": "dev"}
 
 loki_handler = LokiHandler(url=LOKI_URL, labels=LOKI_LABELS, timeout=1.0)
-loki_handler.setLevel(logging.DEBUG)
+loki_handler.setLevel(logging.DEBUG if debug_mode else logging.INFO)
 loki_handler.setFormatter(logging.Formatter(LOG_FMT))
 loki_handler.addFilter(ExcludeLoggerFilter(["urllib3", "requests", "loki.internal"]))
 
