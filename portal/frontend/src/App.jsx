8c09be458b455bf1f84808a1042b7785c068dcda
Elijah Brooks <elijahcbrooks@gmail.com>
2025-07-29 09:47:27 -0500

    alignment for refresh button

--- portal/frontend/src/App.jsx
+++ portal/frontend/src/App.jsx
@@ -28,10 +28,16 @@ function App() {
     <div className="max-w-7xl mx-auto mt-10 p-5 bg-neutral-800 rounded-lg shadow-lg">
 
       {/* controls row */}
-      <div className="flex items-center space-x-4">
+      <div className="flex items-end space-x-4">
         <TimeframeSelect selected={timeframe} onChange={setTimeframe} />
         <SymbolInput     value={symbol}     onChange={setSymbol}     />
         <DateRangePickerComponent />
+        <button className="mt-5.5 self-center border border-neutral-600 rounded-md p-2 hover:bg-neutral-700 transition-colors cursor-pointer">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+          </svg>
+        </button>
+
       </div>
 
       {/* chart + sidebar row */}
