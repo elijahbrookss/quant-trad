479d041cbd4035c01047edf695dc14f55e59b005
Elijah Brooks <elijahcbrooks@gmail.com>
2025-07-29 07:35:19 -0500

    defaults and formatting for date selection

--- portal/frontend/src/components/DateTimePickerComponent.jsx
+++ portal/frontend/src/components/DateTimePickerComponent.jsx
@@ -4,30 +4,52 @@ import "flatpickr/dist/themes/dark.css";
 import Flatpickr from "react-flatpickr";
 
 export function DateRangePickerComponent() {
-  const [date, setDate] = useState([new Date(), new Date()]);
-  const datePickerRef = useRef(null);
+  const [startDate, setStartDate] = useState([new Date(), new Date()]);
+  const [endDate, setEndDate] = useState([new Date(), new Date()]);
 
+  useEffect(() => {
+    // Set default date range to last 45 days
+    const defaultStart = new Date(
+      Date.now() - 45 * 24 * 60 * 60 * 1000
+    ).toISOString().split("T")[0];
+    const defaultEnd = new Date();
+    setStartDate(defaultStart);
+    setEndDate(defaultEnd);
+  }, []);
+
+  const datePickerRef = useRef(null);
 
 	return (
     <div className="flex items-center space-x-3">
       <div className="flex flex-col">
-        <span className="text-sm text-neutral-500"> Start date/time </span>
+        <span className="text-sm text-neutral-500"> Start Date/Time </span>
         <Flatpickr
           ref={datePickerRef}
           data-enable-time
-          value={date}
-          onChange={setDate}
+          value={startDate}
+          onChange={setStartDate}
+          options={{
+            dateFormat: "Y-m-d H:i",
+            time_24hr: true,
+            enableTime: true,
+            minuteIncrement: 1,
+            maxDate: "today",
+            minDate: "2020-01-01",
+            altInput: true,
+            altFormat: "Y-m-d H:i",
+            allowInput: true,
+          }}
           className ="w-fit bg-neutral-800 border border-neutral-600 rounded-md shadow-sm px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
         />
       </div> 
       <span className="text-neutral-400"> to </span>
       <div className="flex flex-col">
-        <span className="text-sm text-neutral-500"> End date/time </span>
+        <span className="text-sm text-neutral-500"> End Date/Time </span>
         <Flatpickr
           ref={datePickerRef}
           data-enable-time
-          value={date}
-          onChange={setDate}
+          value={endDate}
+          onChange={setEndDate}
           className ="w-fit bg-neutral-800 border border-neutral-600 rounded-md shadow-sm px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
         />
       </div> 
