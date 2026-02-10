1e822e439d84bc473b8295037442784bf8f3852c
Elijah Brooks <elijahcbrooks@gmail.com>
2026-02-10 01:24:37 -0600

    infra(docker): simplify postgres bootstrap and wire grafana/postgres/loki provisioning updates

--- docker/postgres/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-SYSTEM_STATS_CONTROL=$(ls /usr/share/postgresql/*/extension/system_stats.control 2>/dev/null || true)
-if [ -z "$SYSTEM_STATS_CONTROL" ]; then
-  echo "system_stats extension not available in this Postgres image; pgAdmin system_stats panel will remain unavailable. Use container stats / cAdvisor instead."
-fi
-
-exec docker-entrypoint.sh "$@"
--- docker/postgres/initdb.d/001_extensions.sql
+++ docker/postgres/initdb.d/001_extensions.sql
@@ -1,5 +1,14 @@
 DO $$
 BEGIN
+  CREATE EXTENSION IF NOT EXISTS timescaledb;
+EXCEPTION
+  WHEN object_not_in_prerequisite_state THEN
+    RAISE WARNING 'timescaledb requires shared_preload_libraries=timescaledb; enable it and restart the server.';
+END;
+$$;
+
+DO $$
+BEGIN
   CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
 EXCEPTION
   WHEN object_not_in_prerequisite_state THEN
@@ -8,12 +17,3 @@ END;
 $$;
 
 CREATE EXTENSION IF NOT EXISTS pg_buffercache;
-
-DO $$
-BEGIN
-  CREATE EXTENSION IF NOT EXISTS system_stats;
-EXCEPTION
-  WHEN undefined_file THEN
-    RAISE WARNING 'system_stats extension not available in this Postgres image.';
-END;
-$$;
